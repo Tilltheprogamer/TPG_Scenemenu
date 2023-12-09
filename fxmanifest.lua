e1d59a24ca506ef920d1509887c05a74d41a5c4e
Tilltheprogamer <118006357+Tilltheprogamer@users.noreply.github.com>
2023-12-09 21:42:14 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Tilltheprogamer'
+
+description 'CHANGEME'
+
+version '1.0'
+
+
+
+client_script {'client/main.lua'
+
+}
+
+server_script {'server/main.lua'
+
+}
+
+shared_scripts {
+
+}
